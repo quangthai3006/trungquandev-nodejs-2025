87326fdfab25aa4f8529d2e4ec8ce6eed717b096
Sniper Boy <quangthai30062002@gmail.com>
2025-04-19 13:38:29 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const port = 8080
+const hostname = 'localhost'
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Hello World, I'm running server at http://${hostname}:${port}`)
+})
